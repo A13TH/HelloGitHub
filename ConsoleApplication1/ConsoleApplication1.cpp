2b91705f2d9694c3285d5d2a05a8dbe142877f11
A13TH <athirteenth@yandex.ru>
2020-05-02 11:30:17 +0600

    doubleNumber

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,15 @@
 ﻿#include<iostream>
+int doubleNumber(int a)
+{
+	return 2 * a;
+}
 int main()
 {
+	int a;
 	std::cout << "Hello World!" << std::endl;
-	return 0;
+	std::cout << "Enter number!" << std::endl;
+	std::cin >> a;
+	std::cout << doubleNumber(a) << std::endl;
+    return 0;
 }
+
